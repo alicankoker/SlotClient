5c0c69520cdcf2e95dd93b9ca8b47882d8423416
Alican Köker <alicankoker@gmail.com>
2025-11-24 17:39:11 +0300

    monorepo setup cleaning

--- src/games/diamondDiggers/config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    BACKEND_URL: import.meta.env.VITE_BACKEND_URL || "https://rngengine.com",
-    USER_ID: import.meta.env.DEV ? "691b0d687091760bfa79b79e" : new URLSearchParams(window.location.search).get("session") || undefined,
-};
