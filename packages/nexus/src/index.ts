8535011deeb5adb3bd18bd5945e9d21fafa5463f
Alican Köker <alicankoker@gmail.com>
2025-11-25 18:26:08 +0300

    removed duplicate files

--- packages/nexus/src/index.ts
+++ packages/nexus/src/index.ts
@@ -1,7 +1,7 @@
 // Nexus package exports
 export * from './Nexus';
 export * from './NexusInterfaces';
-export { PlayerController as NexusPlayerController } from './PlayerController';
+export { PlayerController as NexusPlayerController } from './player/PlayerController';
 export * from './player/PlayerController';
 export * from './player/NexusPlayer';
 export * from './ui/SlotUI';
